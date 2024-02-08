4c6a014eb4e1bbb45f31fcebef5e062266e13924
Azardo5300 <ibrayv2005@gmail.com>
2024-02-08 23:03:28 +0600

    new response helper

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -8,3 +8,18 @@ if (!function_exists('response')) {
     return new Response();
   }
 }
+
+
+if (!function_exists('request')) {
+  function request()
+  {
+    $psr17Factory = new Psr17Factory();
+    $creator = new ServerRequestCreator(
+      $psr17Factory, // ServerRequestFactory
+      $psr17Factory, // UriFactory
+      $psr17Factory, // UploadedFileFactory
+      $psr17Factory  // StreamFactory
+    );
+    return $creator->fromGlobals();
+  }
+}
