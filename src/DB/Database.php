598aad84c6548154b1a2cc12b130ce0b5b068597
Azardo5300 <ibrayv2005@gmail.com>
2024-02-09 22:52:15 +0600

    db fixed

--- src/DB/Database.php
+++ src/DB/Database.php
@@ -40,37 +40,4 @@ class Database implements DatabaseInterface
       throw new RuntimeException("Error executing query: " . $e->getMessage());
     }
   }
-
-  public function faetch(string $sql, array $params = [], $types = [])
-  {
-    $statement = $this->executeQuery($sql, $params, $types);
-    return $statement->fetch(PDO::FETCH_ASSOC);
-  }
-
-
-  public function fetch(PDOStatement $statement): array
-  {
-    try {
-      return $statement->fetch(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-      throw new RuntimeException("Error fetching results: " . $e->getMessage());
-    }
-  }
-
-
-  public function fetchAll(PDOStatement $statement): array
-  {
-    try {
-      return $statement->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-      throw new RuntimeException("Error fetching results: " . $e->getMessage());
-    }
-  }
-
-
-  public function fetchAlll(string $sql, array $params = [], $types = [])
-  {
-    $statement = $this->executeQuery($sql, $params, $types);
-    return $statement->fetchAll(PDO::FETCH_ASSOC);
-  }
 }
